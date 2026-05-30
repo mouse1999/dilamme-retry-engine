3f7fa5bec00fbdb24b1430ba38e7c5b7b5f1ca95
mouse1999 <kufreedward26@gmail.com>
2026-05-30 22:51:31 +0100

    feat: init setup

---
+++ src/main/java/com/mouse/dilamme/retry/DilammeRetryEngineApplication.java
@@ -0,0 +1,13 @@
+package com.mouse.dilamme.retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DilammeRetryEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DilammeRetryEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mouse/dilamme/retry/DilammeRetryEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouse.dilamme.retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DilammeRetryEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
